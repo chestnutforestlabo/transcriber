67c4ede7867159015c89381c2433349336f51018
chestnutforestlabo <rugbykuribayashi@toki.waseda.jp>
2024-08-20 19:37:19 +0900

    jsut convert all data

--- convert_data.sh
+++ convert_data.sh
@@ -6,7 +6,7 @@ for file in data/*.m4a; do
     filename="${file%.m4a}"
     
     # Convert the .m4a file to .wav
-    ffmpeg -i "$file" "${filename}.wav"
+    ffmpeg -i "$file" "${filename}.wav" -y
     
     # Optionally, you can remove the original .m4a file after conversion
     # rm "$file"

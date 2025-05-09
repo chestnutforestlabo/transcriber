aeb137dec28629c6f4a0d669d018b9abaa078ced
shonoshin sakamoto <shoex7@gmaol.com>
2025-05-09 18:14:53 +0900

    add huggingface access token input

--- scripts/backend_setup_2.sh
+++ scripts/backend_setup_2.sh
@@ -1,3 +1,11 @@
 poetry install --no-root --with dev 
 poetry run pip install --upgrade "huggingface_hub[cli]"
-poetry run huggingface-cli login
+
+# Manual login on the terminal
+# poetry run huggingface-cli login
+
+# Log in using environments/backend/.env
+set -a
+source environments/backend/.env
+set +a
+poetry run huggingface-cli login --token "$HF_TOKEN"

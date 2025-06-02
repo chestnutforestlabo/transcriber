f0561dadfb98323bca3ae38f37b04ef709e3786c
chestnutforestlabo <rugbykuribayashi@toki.waseda.jp>
2025-06-02 17:31:30 +0900

    organize scripts

--- scripts/transcribe.sh
+++ scripts/transcribe.sh
@@ -11,6 +11,7 @@ docker compose up backend -d
 
 # Run commands inside the container
 docker compose exec backend bash -c "
-    bash scripts/backend/backend_setup_2.sh
-    bash scripts/backend/backend_transcribe.sh
+    uv sync
+    uv run huggingface-cli login --token "$HF_TOKEN"
+    bash scripts/backend/backend.sh
 "
